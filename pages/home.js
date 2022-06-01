17ffe31b90194faf84bb3f913a38deff7a9b71bd
Meep <mwraich28@gmail.com>
2022-06-01 13:40:57 -0400

    Setup first auth

---
+++ pages/home.js
@@ -0,0 +1,14 @@
+import { useSession } from "next-auth/react"
+
+export default function Home() {
+    const { data: session, status } = useSession()
+
+    if (status === 'loading') {
+        return <p>...</p>
+    }
+    return (
+    <div>
+        {session ? <p> You are logged in!</p> : <p>NOT GRANTED ACCESS</p>}
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/router"
 
 export default function Home() {
-  return (
-    <div>
-      Index
-    </div>
-  )
+  const { data: session, status } = useSession()
+  const router = useRouter()
+
+  if (status === 'loading') {
+    return null
+  }
+  if (session) {
+    router.push('/home')
+  }
+  return <a href='/api/auth/signin'>login</a>
 }
