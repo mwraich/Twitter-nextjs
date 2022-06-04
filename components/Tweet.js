901bcd8a23d170cdd218ec8d5b8ceeda782266a1
Meep <mwraich28@gmail.com>
2022-06-03 21:10:30 -0400

    Setup user profile page

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,19 +1,48 @@
-import Image from 'next/image'
-import timeago from "lib/timeago"
+import Image from 'next/image';
+import timeago from 'lib/timeago';
+import Link from 'next/link';
 
 export default function Tweet({ tweet }) {
-  return (
-    <p>
-      {tweet.author.image && (
-        <Image 
-          className='w-64 h-64 rounded-full'
-          src={tweet.author.image}
-          alt={tweet.author.name}
-          width='40'
-          height='40'
-        />
-      )}
-      {`${timeago.format(new Date(tweet.createdAt))} ${tweet.content} ${tweet.author.name}`}
-    </p>
-  )
+	return (
+		<>
+			<div className="mb-4">
+				<div className="flex flex-shrink-0 p-4 pb-0">
+					<div className="flex-shrink-0 block group">
+						<div className="flex items-center">
+							{tweet.author.image && (
+								<Image
+									className="w-64 h-64 rounded-full"
+									src={tweet.author.image}
+									alt={tweet.author.name}
+									width="40"
+									height="40"
+								/>
+							)}
+							<div className="ml-3">
+								<p>
+									<Link href={`/${tweet.author.name}`}>
+										<a>
+											<span className="text-base font-medium leading-6 color-primary hover:underline">
+												{tweet.author.name}
+											</span>
+										</a>
+									</Link>
+									<span className="pl-1 text-sm font-light leading-5 color-dimmed">
+										<a className="hover:underline">
+											{`${timeago.format(new Date(tweet.createdAt))}`}
+										</a>
+									</span>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="pl-16 mt-6">
+				<p className="flex-shrink pl-1 pr-2 text-base font-normal color-primary width-auto">
+					{tweet.content}
+				</p>
+			</div>
+		</>
+	);
 }
