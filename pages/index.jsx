edd86ab99e9b09568df950d5f90b901649d9e935
Meep <mwraich28@gmail.com>
2022-06-03 21:35:01 -0400

    Setup homepage

---
+++ pages/index.jsx
@@ -0,0 +1,39 @@
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/router"
+import Tweets from "components/Tweets"
+import prisma from "lib/prisma"
+import { getTweets } from "lib/data"
+export default function Welcome({ tweets }) {
+  const { data: session, status } = useSession()
+  const router = useRouter()
+
+  if (status === 'loading') {
+    return null
+  }
+  if (session) {
+    router.push('/home')
+  }
+  return(
+    <div className="mt-10">
+      <Tweets tweets={tweets} />
+      <div className="text-center p-4 border m-4">
+        <h2 className="mb-10">Join the Conversation!</h2>
+        <a 
+          className="border px-8 py-2 mt-5 font-bold rounded-full color-accent-contrast bg-color-accent hover:bg-color-accent-hover-darker"
+        href='/api/auth/signin'>login</a>
+      </div>
+    </div>
+  )}
+
+
+  export async function getServerSideProps() {
+    const take = 4
+    let tweets = await getTweets(prisma, take)
+    tweets = JSON.parse(JSON.stringify(tweets))
+
+    return {
+      props: {
+        tweets
+      }
+    }
+  }
